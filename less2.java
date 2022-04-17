ae06aee230a8b79ce523fbd3b8f99e97dc63eaac
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-17 22:05:43 +0300

    двійкова система

---
+++ less2.java
@@ -0,0 +1,10 @@
+public class less2 {
+
+    public static void main(String args[]){
+        int x = 0b001;
+        int y = 0b00;
+        int q = x + y;
+        System.out.println(q);
+
+    }
+}
