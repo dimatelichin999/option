f294766f19ff58b0cd2a9e792c2013a8b39aeebd
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-17 21:44:32 +0300

    восьмомична кодировка

---
+++ less1.java
@@ -0,0 +1,10 @@
+public class less1 {
+
+    public static void main(String args[]){
+         int x = 0125;
+         int y = 800;
+         int q = x + y;
+         System.out.println(q);
+
+    }
+}
