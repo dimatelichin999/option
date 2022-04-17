6069e1f2f65ec26b163868c7e73160985fd5022c
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-17 22:29:59 +0300

    шіснацяткова система

---
+++ less3.java
@@ -0,0 +1,10 @@
+public class less3 {
+
+    public static void main(String args[]){
+        int x = 0xABC100;
+        int y = 0xBCF10;
+        int q = x + y;
+        System.out.println(q);
+
+    }
+}
